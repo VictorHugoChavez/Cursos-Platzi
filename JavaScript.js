fa81450514137511762f3bfb5e05efaf1b015969
VictorHugoChavez <96146923+VictorHugoChavez@users.noreply.github.com>
2022-09-01 22:30:31 -0500

    Create JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,23 @@
+// Javascript es un lenguaje interpredado, orientado a objetos, debilmente tipado y dinamico
+
+//Debilmente Tipado
+
+4+"7";  // 47
+4*"7";   //28
+2+true; //3
+false-3; //-3
+
+//Dinamico lenguajes donde corre en RunTime 
+//Motor Javascript V8
+
+//Backwars
+//La compatibilidd con versiones anteriores significa cuando se acepta como JS valido, no habra un cambio de futuro que haga que el codigo deje de funcionar
+// Forwards Ser compatible con versiones futuras significa que incluir una adicion al lenguaje en un programa no causaria que se rompa si se ejecuta en un motor JS anterior
+
+//BABEL
+
+//Copiladores de JS, donde utiliza versiones nuevas y utiliza nuevas
+
+//HTML, CSS, JS
+// WA ya no requiere tanto productos web
+//Backend- FrontEnd
