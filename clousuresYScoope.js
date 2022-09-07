a6ea435af122430306e563ae6232afbc03fc8637
VictorHugoChavez <96146923+VictorHugoChavez@users.noreply.github.com>
2022-09-07 18:20:23 -0500

    Update clousuresYScoope.js

--- clousuresYScoope.js
+++ clousuresYScoope.js
@@ -74,5 +74,49 @@ console.log(pi);
 
 
 
+¿La herramienta de Google Chrome que nos permite hacer debugging se llama?
+Chrome DevTools
+
+JavaScript solo utiliza el hoisting en declaraciones, mas no en inicializaciones
+Verdadero
+
+Analiza el siguiente código e identifica la variable declarada en el alcance de la función:
+
+const fruits = () => { if (true) { var fruit1 = 'apple'; const fruit2 = 'banana'; let fruit3 = 'kiwi'; } }
+var fruit1 = ‘apple’;
+
+Las variables declaradas con let y const, NO se pueden volver a declarar en el mismo ámbito.
+Verdadero
+
+¿En qué momento se genera una closure?
+Cuando una función accede a una variable fuera de su contexto y la recuerda.
+
+Es el tipo de dato que se asigna por defecto al declarar una variable.
+Undefined
+
+
+Variables declaradas con "let" y "const" son de "block scope"
+Verdadero
+
+¿Cuál es el alcance que tiene el function scope?
+Se puede acceder a una variable que se ubica dentro de una función, pero no podemos llamarla desde el ámbito global.
+
+Término que se refiere a la accesibilidad que tiene una variable, función u objeto.
+Scope
+
+
+
+/////////////
+¿Qué sucederá si ejecutamos el siguiente código? ``` nameOfDog("Elmo"); function nameOfDog(name) { console.log(name); }; ```
+Se muestra en consola el siguiente mensaje: "Variable name undefined".
+
+¿Cuál de las siguientes opciones NO se puede considerar como una variable global?
+let scope = "I am global";`
+
+¿Para qué se utiliza el modo estricto en JavaScript?
+Lo aplicamos para generar variables inmutables de forma estricta.
+
+¿Qué es Code Runner?
+Es un plugin de Visual Studio Code que nos habilita el uso del scope y los closures dentro del código.
 
 
