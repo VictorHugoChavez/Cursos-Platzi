117b8a0cc847bb62e8d40feac406f76ebfcbfb8a
VictorHugoChavez <96146923+VictorHugoChavez@users.noreply.github.com>
2022-09-18 15:37:03 -0500

    add: line185-203 in challenges.js

--- promise/challenge.js
+++ promise/challenge.js
@@ -32,90 +32,84 @@ promise2().then((result) => {
     promise1().then((result) => console.log(result));
 });
 
-
 const employe = {
-        firstName: 'Pedro',
-        age: 30,
-        phone: 55516453,
-    }
-    //const street = employe ? .street;
+    firstName: "Pedro",
+    age: 30,
+    phone: 55516453,
+};
+//const street = employe ? .street;
 
 console.log(street);
 
 const array = [21, 13, 34, 26, 2, 20];
-const foundNumber = array.find(number => number > 20);
+const foundNumber = array.find((number) => number > 20);
 console.log(foundNumber);
 
-let transportes = ['avion', 'coche', 'metro', 'microbus']
+let transportes = ["avion", "coche", "metro", "microbus"];
 console.log(transportes.shift(5));
 
-let a = 10
+let a = 10;
 if (a == "10") {
     console.log("Valor1");
 } else {
-    console.log("Valor2")
+    console.log("Valor2");
 }
-let nombre = 'Victor/Ernesto/Carlos/Maria/Viri'
-let aNombre = nombre.split('');
+let nombre = "Victor/Ernesto/Carlos/Maria/Viri";
+let aNombre = nombre.split("");
 console.log(aNombre.length);
-
 
 function f(x, y = 2, z = 7) {
     return x + y + z;
 }
-console.log(f(1) === 10)
+console.log(f(1) === 10);
 
 let objeto1 = {
-    name: 'Carlos',
-    lastname: 'Alberto',
+    name: "Carlos",
+    lastname: "Alberto",
     age: 25,
-}
+};
 var json = JSON.stringify(objeto1);
 console.log(json);
 
-console.log('°-°'.padEnd(13, '-').padStart(10, '-'))
+console.log("°__°".padEnd(10, "-").padStart(13, "-"))
+console.log("°__°".padStart(10, "-").padEnd(13, "-"))
+    (function f(f) {
+        return typeof f();
+    })(function() {
+        return 1;
+    });
 
+console.log(f());
 
-(function f(f) {
-    return typeof f()
-})
-(function() { return 1; })
-
-console.log(f())
-
-var jsonTexto = '{"color":"blanco","km":10000,"rueda":{"marca":"desconocida","estado":"malo"}}';
+var jsonTexto =
+    '{"color":"blanco","km":10000,"rueda":{"marca":"desconocida","estado":"malo"}}';
 var coche = JSON.parse(jsonTexto);
-console.log(coche)
-
+console.log(coche);
 
 const director = {
-    name: 'Gabriel Sosa',
-    compania: 'Accenture',
+    name: "Gabriel Sosa",
+    compania: "Accenture",
     age: 32,
-    phonenumber: 6543213215
+    phonenumber: 6543213215,
+};
+const alumno = {
+    name: 'Ernesto',
+    lastname: 'Antonio',
+    age: 29
 }
-
+const nuevoDirector = {...director, ...alumno }
+console.log(nuevoDirector);
 const nuevoDirector = {...director };
-director.name = 'Antonio Jaimes'
-console.log(nuevoDirector)
-
+director.name = "Antonio Jaimes";
+console.log(nuevoDirector);
 
 const arreglo = [1, 2, 3, 4];
 const arreglo2 = [10, 16, 20, 21];
 
-let valor = [...arreglo, arreglo2]
-console.log(valor)
-
-[1, 2, 3, 4, 10, 12, 16, 19, 20, 21, 22, 100]
-[
-    [1, 2, 3, 4],
-    [10, 16, 20, 21],
-    [100, 12, 19, 22]
-]
-[
-    [1, 2, 3, 4],
-    [10, 16, 20, 21]
-]
+let valor = [...arreglo, arreglo2];
+console.log(valor)[(1, 2, 3, 4, 10, 12, 16, 19, 20, 21, 22, 100)][
+    ([1, 2, 3, 4], [10, 16, 20, 21], [100, 12, 19, 22])
+][([1, 2, 3, 4], [10, 16, 20, 21])];
 
 class ope {
     static ave(arre) {
@@ -126,18 +120,18 @@ class ope {
         return res / arre.length;
     }
 }
-let suma = ope.ave([5, 10, 15])
-console.log(suma)
+let suma = ope.ave([5, 10, 15]);
+console.log(suma);
 
 var a;
 var b = a == null;
 
-console.log(b)
+console.log(b);
 
-const event = new CustomEvent('my-event', { detail: { message: "Something happend" } });
+const event = new CustomEvent("my-event", {
+    detail: { message: "Something happend" },
+});
 this.dispatchEvent(event);
-
-
 
 var p1 = new Promise((resolve, reject) => {
     setTimeout(resolve, 5000, "promise1");
@@ -152,11 +146,58 @@ var p4 = new Promise((resolve, reject) => {
     setTimeout(resolve, 4000, "promise4");
 });
 
+Promise.all([p2, p1, p4, p3]).then((values) => {
+    console.log(values);
+});
 
-Promise.all([p2, p1, p4, p3]).then(values => { console.log(values); });
+promise2;
+promise1;
+promise4;
+promise3;
+
+import { LitElement, html } from "lit";
+export class MyElement extends LitElement {
+    static get properties() {
+        return {
+            state: { type: Object },
+        };
+    }
+    constructor() {
+        super();
+        this.state = { seconds: 0 };
+
+        this.interval = setInterval(
+            () =>
+            (this.state = {
+                seconds: this.state.seconds + 1,
+            }),
+            1000
+        );
+    }
+
+    render() {
+        return html ` <div>Count: ${this.state.seconds}</div> `;
+    }
+}
+customElements.define("my-element", MyElement);
 
 
-promise2
-promise1
-promise4
-promise3
+import { html, LitElement } from 'lit';
+export class BBComponent extends LitElement {
+    static get properties() {
+        return { object: { type: Object } }
+    }
+    constructor() {
+        super();
+        this.object = { myProp: 'Initial Value' };
+    }
+    handleClick() {
+        this.object.myProp = 'Current value';
+    }
+    render() {
+        return html `
+        <h2>${this.object.myProp}</h2>
+        <button @click=${this.handleClick}>Click me!</button>
+        `;
+    }
+}
